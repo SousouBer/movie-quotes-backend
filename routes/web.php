4176284c8abed2df000614ed8b661af3393f29ec
Soso <soso.beriashvili@redberry.ge>
2024-05-15 21:48:56 +0400

    refactor: create google auth controller

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,11 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\GoogleAuthController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Socialite\Facades\Socialite;
-use Illuminate\Support\Str;
 
 Route::get('/', function () {
 	return view('welcome');
 });
 
-Route::get('/auth/google/redirect', function () {
-	return Socialite::driver('google')->redirect();
-});
-
-Route::get('/auth/google/callback', function (Request $request) {
-	$googleUser = Socialite::driver('google')->user();
-
-	$existingUser = User::whereFirst('google_id', $googleUser->id);
-
-	$googleUser = User::updateOrCreate([
-		'google_id' => $googleUser->id,
-	], [
-		'username'                 => $googleUser->name,
-		'email'                    => $googleUser->email,
-		'password'                 => Str::password(10),
-		'email_verified_at'        => now(),
-	]);
-
-	Auth::login($googleUser);
-
-	// I think I will change this response here.
-	return redirect(config('app.frontend_url') . '/landing');
-});
+Route::get('/auth/google/redirect', [GoogleAuthController::class, 'redirect'])->name('auth.google_redirect');
+Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback'])->name('auth.google_callback');
