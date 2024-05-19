a6e6d572f00ab6a8f20780707a69e7b1532dc62e
Soso <soso.beriashvili@redberry.ge>
2024-05-19 22:56:49 +0400

    feat: add verified middleware to check email verification

---
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureEmailIsVerified
+{
+	public function handle(Request $request, Closure $next): Response
+	{
+		$credential = $request->input('username_or_email');
+
+		$field = filter_var($credential, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+		$user = User::where($field, $credential)->first();
+
+		if ($user && !$user->hasVerifiedEmail()) {
+			return response()->json(['username_or_email' => 'Your email address is not verified'], 409);
+		}
+
+		return $next($request);
+	}
+}
