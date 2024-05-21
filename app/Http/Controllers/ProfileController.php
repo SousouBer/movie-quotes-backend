f12ccc52c43ec9b82431fbc470775bb97ade3d75
Soso <soso.beriashvili@redberry.ge>
2024-05-22 00:56:50 +0400

    feat: add changing username and password logic

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UpdateProfileRequest;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
 	public function update(UpdateProfileRequest $request): JsonResponse
 	{
 		$fields = $request->validated();
+
+		$user = Auth::user();
+
+		if ($request->filled('username')) {
+			$user->username = $fields['username'];
+		}
+
+		if ($request->filled('password')) {
+			$user->password = Hash::make($fields['password']);
+		}
+
+		$user->save();
 
 		return response()->json(['message' => 'User profile was successfully changed', 201]);
 	}
