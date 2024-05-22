075e709c173647eb4196b25b28f95000a75fb193
Soso <soso.beriashvili@redberry.ge>
2024-05-22 12:38:07 +0400

    feat: add more validation to user avatar image

--- app/Http/Requests/UpdateProfileRequest.php
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -9,7 +9,7 @@ class UpdateProfileRequest extends FormRequest
 	public function rules(): array
 	{
 		return [
-			'avatar'    => 'sometimes|image',
+			'avatar'    => 'sometimes|image|mimes:jpg,jpeg,png,gif',
 			'username'  => 'sometimes|min:3|max:15|lowercase|alpha_num|unique:users,username',
 			'password'  => 'sometimes|min:8|max:15|lowercase|alpha_num|confirmed',
 		];
