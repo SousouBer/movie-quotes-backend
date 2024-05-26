e1db25deb12b05d479607831584b57a609c49640
Soso <soso.beriashvili@redberry.ge>
2024-05-27 02:52:10 +0400

    feat: return resources in controllers

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\MovieResource;
+use App\Models\Movie;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class MovieController extends Controller
 {
-    //
+	public function index(Request $request): AnonymousResourceCollection
+	{
+		return MovieResource::collection(Movie::all());
+	}
+
+	public function show(Movie $movie): MovieResource
+	{
+		return MovieResource::make($movie);
+	}
 }
