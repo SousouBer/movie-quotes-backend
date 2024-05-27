f0559294dab53217725ada77405ccb8dc8ad2784
Soso <soso.beriashvili@redberry.ge>
2024-05-27 16:52:27 +0400

    feat: return accidentally deleted index functions

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\MovieResource;
 use App\Models\Movie;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class MovieController extends Controller
 {
+	public function index(): AnonymousResourceCollection
+	{
+		return MovieResource::collection(Movie::all());
+	}
+
 	public function show(Movie $movie): MovieResource
 	{
 		return MovieResource::make($movie);
--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\QuoteResource;
 use App\Models\Quote;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class QuoteController extends Controller
 {
+	public function index(): AnonymousResourceCollection
+	{
+		return QuoteResource::collection(Quote::all());
+	}
+
 	public function show(Quote $quote): QuoteResource
 	{
 		return QuoteResource::make($quote);
