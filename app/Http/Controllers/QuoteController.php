2af1672ced58ec7ed68584302a50c5f73d7f5969
Soso <soso.beriashvili@redberry.ge>
2024-05-27 14:13:14 +0400

    chore: remove unnecessary requests from movie and quote controller

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -4,16 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\QuoteResource;
 use App\Models\Quote;
-use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class QuoteController extends Controller
 {
-	public function index(Request $request): AnonymousResourceCollection
-	{
-		return QuoteResource::collection(Quote::all());
-	}
-
 	public function show(Quote $quote): QuoteResource
 	{
 		return QuoteResource::make($quote);
