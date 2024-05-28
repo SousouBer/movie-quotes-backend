ea2ea9ac4671699251240e0b4f90a3ed6e4a8203
Soso <soso.beriashvili@redberry.ge>
2024-05-28 16:39:34 +0400

    feat: add genre translations

--- config/genres.php
+++ config/genres.php
@@ -2,45 +2,113 @@
 
 return [
 	'genres' => [
-		'Action',
-		'Adventure',
-		'Animated',
-		'Biography',
-		'Comedy',
-		'Crime',
-		'Dance',
-		'Disaster',
-		'Documentary',
-		'Drama',
-		'Erotic',
-		'Family',
-		'Fantasy',
-		'Found Footage',
-		'Historical',
-		'Horror',
-		'Independent',
-		'Legal',
-		'Live Action',
-		'Martial Arts',
-		'Musical',
-		'Mystery',
-		'Noir',
-		'Performance',
-		'Political',
-		'Romance',
-		'Satire',
-		'Science Fiction',
-		'Short',
-		'Silent',
-		'Slasher',
-		'Sports',
-		'Spy',
-		'Superhero',
-		'Supernatural',
-		'Suspense',
-		'Teen',
-		'Thriller',
-		'War',
-		'Western',
+		[
+			'en' => 'Action',
+			'ka' => 'ექშენი',
+		],
+		[
+			'en' => 'Adventure',
+			'ka' => 'სათავგადასავლო',
+		],
+		[
+			'en' => 'Animated',
+			'ka' => 'ანიმაციური',
+		],
+		[
+			'en' => 'Biography',
+			'ka' => 'ბიოგრაფიული',
+		],
+		[
+			'en' => 'Comedy',
+			'ka' => 'კომედია',
+		],
+		[
+			'en' => 'Crime',
+			'ka' => 'კრიმინალური',
+		],
+		[
+			'en' => 'Dance',
+			'ka' => 'ცეკვა',
+		],
+		[
+			'en' => 'Disaster',
+			'ka' => 'დასავლეთები',
+		],
+		[
+			'en' => 'Documentary',
+			'ka' => 'დოკუმენტური',
+		],
+		[
+			'en' => 'Drama',
+			'ka' => 'დრამა',
+		],
+		[
+			'en' => 'Family',
+			'ka' => 'საოჯახო',
+		],
+		[
+			'en' => 'Fantasy',
+			'ka' => 'ფენტეზი',
+		],
+		[
+			'en' => 'Found Footage',
+			'ka' => 'რეალური ჩანაწერები',
+		],
+		[
+			'en' => 'Historical',
+			'ka' => 'ისტორიული',
+		],
+		[
+			'en' => 'Horror',
+			'ka' => 'საშინელებათა',
+		],
+		[
+			'en' => 'Live Action',
+			'ka' => 'ლაივ ექშენი',
+		],
+		[
+			'en' => 'Martial Arts',
+			'ka' => 'საბრძოლო ხელოვნება',
+		],
+		[
+			'en' => 'Musical',
+			'ka' => 'მიუზიკლი',
+		],
+		[
+			'en' => 'Mystery',
+			'ka' => 'მისტიკა',
+		],
+		[
+			'en' => 'Political',
+			'ka' => 'პოლიტიკური',
+		],
+		[
+			'en' => 'Romance',
+			'ka' => 'რომანტიკა',
+		],
+		[
+			'en' => 'Sports',
+			'ka' => 'სპორტული',
+		],
+		[
+			'en' => 'Supernatural',
+			'ka' => 'ზებუნებრივი',
+		],
+		[
+			'en' => 'Suspense',
+			'ka' => 'სუსპენსი',
+		],
+		[
+			'en' => 'Teen',
+			'ka' => 'თინეიჯერი',
+		],
+		[
+			'en' => 'Thriller',
+			'ka' => 'თრილერი',
+		],
+		[
+			'en' => 'War',
+			'ka' => 'ომი',
+		],
 	],
 ];
