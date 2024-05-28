e27e3d2a8f94bdc22e9537d4a0b2ef4f356678a5
Soso <soso.beriashvili@redberry.ge>
2024-05-28 16:50:20 +0400

    feat: add genre seeder and seed data through iy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,16 +8,18 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+	/**
+	 * Seed the application's database.
+	 */
+	public function run(): void
+	{
+		$this->call([
+			GenreSeeder::class,
+		]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+		User::factory()->create([
+			'username'  => 'Test User',
+			'email'     => 'test@example.com',
+		]);
+	}
 }
