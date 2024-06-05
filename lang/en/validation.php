98f80f787df4fab1e0b212012817389a7366f00d
Soso <soso.beriashvili@redberry.ge>
2024-06-05 15:51:28 +0400

    feat: add validation translations

---
+++ lang/en/validation.php
@@ -0,0 +1,9 @@
+
+<?php
+
+return [
+	'required'               => 'The :attribute is required',
+	'georgian_letters'       => 'Only Georgian letters are allowed',
+	'english_letters'        => 'Only English letters are allowed.',
+	'confirmed'              => 'New passwords do not match. Try again',
+];
