54bfe170dc694078e1be6f6a0698d04657cfd2a8
Soso <soso.beriashvili@redberry.ge>
2024-06-07 17:41:27 +0400

    feat: add middleware to switch between locales using cookies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\SetLocale;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
 		$middleware->alias([
 			'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
 		]);
+
+		$middleware->append(SetLocale::class);
 	})
 	->withExceptions(function (Exceptions $exceptions) {
 	})->create();
