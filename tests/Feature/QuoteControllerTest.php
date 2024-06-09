7194693deefda308260142bb066b87267d036491
Soso <soso.beriashvili@redberry.ge>
2024-06-10 03:18:42 +0400

    test: add tests for quote related crud operations

---
+++ tests/Feature/QuoteControllerTest.php
@@ -0,0 +1,132 @@
+<?php
+
+use App\Models\Movie;
+use App\Models\Quote;
+use App\Models\User;
+use Illuminate\Http\UploadedFile;
+
+test('New quote is successfully added', function () {
+	$user = User::factory()->create();
+	$movie = Movie::inRandomOrder()->firstOrFail();
+
+	$this->actingAs($user);
+
+	$picture = UploadedFile::fake()->image('quotePicture.png');
+
+	$quoteData = [
+		'user_id'  => $user->id,
+		'movie_id' => $movie->id,
+		'quote'    => [
+			'en' => 'Quote English',
+			'ka' => 'ციტატა ქართულად',
+		],
+		'picture' => $picture,
+	];
+
+	$response = $this->post(route('quotes.store'), $quoteData);
+
+	$response->assertStatus(201)->assertJson(['message' => 'Quote added successfully']);
+
+	$response->assertSessionDoesntHaveErrors(
+		[
+			'quote.en',
+			'quote.ka',
+			'picture',
+		]
+	);
+
+	$this->assertDatabaseHas('quotes', [
+		'quote->en' => 'Quote English',
+		'quote->ka' => 'ციტატა ქართულად',
+	]);
+});
+
+test('New quote can not be added if no picture is provided', function () {
+	$user = User::factory()->create();
+	$movie = Movie::inRandomOrder()->firstOrFail();
+
+	$this->actingAs($user);
+
+	$quoteData = [
+		'user_id'  => $user->id,
+		'movie_id' => $movie->id,
+		'quote'    => [
+			'en' => 'Quote English Test',
+			'ka' => 'ციტატა ქართულად სატესტო',
+		],
+	];
+
+	$response = $this->post(route('quotes.store'), $quoteData);
+
+	$response->assertStatus(302);
+
+	$response->assertSessionDoesntHaveErrors(
+		[
+			'quote.en',
+			'quote.ka',
+		]
+	);
+
+	$response->assertSessionHasErrors(
+		[
+			'picture',
+		]
+	);
+
+	$this->assertDatabaseMissing('quotes', [
+		'quote->en' => $quoteData['quote']['en'],
+		'quote->ka' => $quoteData['quote']['ka'],
+	]);
+});
+
+test('Quote can be successfully updated', function () {
+	$user = User::factory()->create();
+	$quote = Quote::inRandomOrder()->firstOrFail();
+
+	$this->actingAs($user);
+
+	$newPicture = UploadedFile::fake()->image('newQuotePicture.png');
+
+	$editedQuoteData = [
+		'quote'    => [
+			'en' => 'Quote English not edited',
+			'ka' => 'ციტატა ქართულად შეუცვლელი',
+		],
+		'picture' => $newPicture,
+	];
+
+	$response = $this->patch(route('quotes.update', ['quote' => $quote->id]), $editedQuoteData);
+
+	$response->assertStatus(201)->assertJson(['message' => 'Quote updated successfully']);
+
+	$response->assertSessionDoesntHaveErrors(
+		[
+			'quote.en',
+			'quote.ka',
+			'picture',
+		]
+	);
+
+	$this->assertEquals(Quote::findOrFail($quote->id)->quote, $editedQuoteData['quote']['en']);
+});
+
+test('Quote can be successfully deleted', function () {
+	$user = User::factory()->create();
+	$quote = Quote::inRandomOrder()->firstOrFail();
+
+	$this->actingAs($user);
+
+	$response = $this->delete(route('quotes.update', ['quote' => $quote->id]));
+
+	$response->assertStatus(200)->assertJson(['message' => 'Quote deleted successfully']);
+
+	$response->assertSessionDoesntHaveErrors(
+		[
+			'quote.en',
+			'quote.ka',
+			'picture',
+		]
+	);
+
+	$this->assertDatabaseMissing('quotes', ['id' => $quote->id]);
+});
