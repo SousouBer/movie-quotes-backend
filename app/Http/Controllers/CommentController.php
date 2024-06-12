1ced5a29db5e3123d88ffb26f3e7f96c7a65788f
Soso <soso.beriashvili@redberry.ge>
2024-06-12 17:31:05 +0400

    feat: create notification's event and broadcast

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\userNotification;
 use App\Http\Requests\StoreCommentRequest;
 use App\Http\Resources\QuoteResource;
 use App\Models\Comment;
@@ -14,6 +15,15 @@ class CommentController extends Controller
 		Comment::create($request->validated());
 
 		$updatedQuote = Quote::findOrFail($request->input('quote_id'));
+
+		// I will also change this data, and create table for notifications in the next branch.
+		$notification = [
+			'comment_author_id' => $request->validated()['user_id'],
+			'quote_author_id'   => $updatedQuote->user->id,
+			'message'           => 'Your quote has been commented!',
+		];
+
+		userNotification::dispatch($notification);
 
 		return QuoteResource::make($updatedQuote);
 	}
