7cd9937e94b38e4f1aa79e70d7a8a5ed2ec2d085
Soso <soso.beriashvili@redberry.ge>
2024-06-13 23:21:17 +0400

    feat: add and group sanctum protected routes with the middleware

--- routes/api.php
+++ routes/api.php
@@ -24,38 +24,38 @@ Route::middleware('guest')->group(function () {
 	Route::post('/forgot-password', [PasswordController::class, 'forgotPassword'])->name('password.forgot');
 	Route::post('/resend-email-verification/{email}', [EmailController::class, 'resendEmailVerification'])->name('email.resend_verification');
 	Route::post('/reset-password/{email}/{token}', [PasswordController::class, 'resetPassword'])->name('password.reset');
+	Route::get('/email-verify/{id}/{hash}', [EmailController::class, 'verifyEmail'])->middleware(['signed'])->name('verification.verify');
 });
 
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
+Route::middleware('auth:sanctum')->group(function () {
+	Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
+	Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update')->middleware('auth:sanctum');
 
-Route::get('/email-verify/{id}/{hash}', [EmailController::class, 'verifyEmail'])->middleware(['signed'])->name('verification.verify');
+	Route::controller(MovieController::class)->prefix('movies')->group(function () {
+		Route::get('/', 'index')->name('movies.index');
+		Route::get('/{movie}', 'show')->name('movies.show');
+		Route::post('/', 'store')->name('movies.store');
+		Route::get('/edit/{movie}', 'edit')->name('movies.edit');
+		Route::patch('/{movie}', 'update')->name('movies.update');
+		Route::delete('/{movie}', 'destroy')->name('movies.destroy');
+	});
 
-Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update')->middleware('auth:sanctum');
+	Route::get('/genres', [GenreController::class, 'index'])->name('genres.index');
+	Route::post('/comment', [CommentController::class, 'store'])->name('comments.store');
 
-Route::controller(MovieController::class)->prefix('movies')->group(function () {
-	Route::get('/', 'index')->name('movies.index');
-	Route::get('/{movie}', 'show')->name('movies.show');
-	Route::post('/', 'store')->name('movies.store');
-	Route::get('/edit/{movie}', 'edit')->name('movies.edit');
-	Route::patch('/{movie}', 'update')->name('movies.update');
-	Route::delete('/{movie}', 'destroy')->name('movies.destroy');
-});
+	Route::controller(QuoteController::class)->prefix('quotes')->group(function () {
+		Route::get('/', 'index')->name('quotes.index');
+		Route::get('/{quote}', 'show')->name('quotes.show');
+		Route::post('/', 'store')->name('quotes.store');
+		Route::patch('/{quote}', 'update')->name('quotes.update');
+		Route::get('/edit/{quote}', 'edit')->name('quotes.edit');
+		Route::delete('/{quote}', 'destroy')->name('quotes.destroy');
+		Route::post('/like', 'like')->name('quotes.like');
+	});
 
-Route::get('/genres', [GenreController::class, 'index'])->name('genres.index');
-Route::post('/comment', [CommentController::class, 'store'])->name('comments.store');
-
-Route::controller(QuoteController::class)->prefix('quotes')->group(function () {
-	Route::get('/', 'index')->name('quotes.index');
-	Route::get('/{quote}', 'show')->name('quotes.show');
-	Route::post('/', 'store')->name('quotes.store');
-	Route::patch('/{quote}', 'update')->name('quotes.update');
-	Route::get('/edit/{quote}', 'edit')->name('quotes.edit');
-	Route::delete('/{quote}', 'destroy')->name('quotes.destroy');
-	Route::post('/like', 'like')->name('quotes.like');
-});
-
-Route::controller(NotificationController::class)->prefix('notifications')->group(function () {
-	Route::get('/', 'index')->name('notifications.index');
-	Route::get('/{notification}/mark-as-read', 'markRead')->name('notifications.markRead');
-	Route::get('/mark-all-as-read', 'markAllAsRead')->name('notifications.markAllAsRead');
+	Route::controller(NotificationController::class)->prefix('notifications')->group(function () {
+		Route::get('/', 'index')->name('notifications.index');
+		Route::get('/{notification}/mark-as-read', 'markRead')->name('notifications.markRead');
+		Route::get('/mark-all-as-read', 'markAllAsRead')->name('notifications.markAllAsRead');
+	});
 });
