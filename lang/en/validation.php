69d57a29f32b08be60c9c527ab42a2bcee0eff15
Soso <soso.beriashvili@redberry.ge>
2024-06-19 17:56:43 +0400

    fix: fix and add validation messages for big and wrong size images

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -2,12 +2,13 @@
 <?php
 
 return [
-	'required'               => 'The :attribute is required',
-	'georgian_letters'       => 'Only Georgian letters are allowed',
-	'english_letters'        => 'Only English letters are allowed.',
-	'confirmed'              => 'New passwords do not match. Try again',
-	'invalid_credentials'    => 'User with provided credentials has not been found',
-	'has_google_account'     => 'User with provided credendial has a Google account',
-	'unverified_email'       => 'The email address is not verified',
-	'login_with_google'      => 'Use your Google account to log in instead',
+	'required'                   => 'The :attribute is required',
+	'georgian_letters'           => 'Only Georgian letters are allowed',
+	'english_letters'            => 'Only English letters are allowed.',
+	'confirmed'                  => 'New passwords do not match. Try again',
+	'invalid_credentials'        => 'User with provided credentials has not been found',
+	'has_google_account'         => 'User with provided credendial has a Google account',
+	'unverified_email'           => 'The email address is not verified',
+	'login_with_google'          => 'Use your Google account to log in instead',
+	'uploaded'                   => 'The :attribute failed to upload. Try lower size image.',
 ];
